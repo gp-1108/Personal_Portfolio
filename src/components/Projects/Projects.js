7b06f9aab10de501237f9aa60750806b1bb5fc1a
gp-1108 <pietrogirotto00@gmail.com>
2024-08-16 13:35:47 +0200

    added two of my projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,9 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import nlp_rag from "../../Assets/Projects/nlp_rag.jpg";
+import gym_app from "../../Assets/Projects/gym_app.jpg";
 
 function Projects() {
   return (
@@ -23,23 +23,22 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={nlp_rag}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="NLP RAG"
+              description="Developed a RAG system using Langchain, Llama3, and FAISS to answer questions about Kendrick Lamar's life and career. The project covers everything from dataset creation to model usage. Demonstrates end-to-end NLP pipeline implementation with a focus on efficiency and practical application."
+              ghLink="https://github.com/gp-1108/NLP_RAG"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={gym_app}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Automating Gym Booking"
+              description="My climbing gym requires booking a slot in advance. I built a bot that automates the booking process. The bot uses webscrapers to interact with the gym's website and books a slot based on user preferences with scheduled cloud functions."
+              ghLink="https://github.com/gp-1108/gym-bros"
+              demoLink="https://my-gym-bot.web.app/"
             />
           </Col>
 
